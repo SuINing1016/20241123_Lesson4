0af14ed415c1b1098e11eb0f59b1d3029aa77540
SuINing <IningSu@gmail.com>
2024-12-07 16:17:18 +0800

    lesson6_5

--- lesson6/lesson6_4.py
+++ lesson6/lesson6_4.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import tools
-sitenames:list[str]=tools.get_sitenames(excel_name='aqi.xlsx')
 
+sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
+# Object notation
 #add_selectbox = st.sidebar.selectbox(
-#   "請選擇站點名稱：",
-#    sitenames
+#    "請選擇站點名稱:",
+#    sitenames 
 #)
+
+# "with" notation
+
 with st.sidebar:
     add_selectbox = st.selectbox(
        "請選擇站點名稱:",
@@ -13,3 +17,16 @@ with st.sidebar:
     )
 
     st.title(f"{add_selectbox}")
+
+
+allData:list[dict] = tools.get_aqi(excel_name='aqi.xlsx')
+
+
+#selected_item:list[dict] = []
+#for item in allData:
+#    if item['sitename'] == add_selectbox:
+#        selected_item.append(item)
+#'''
+
+selected_item:list[dict] = [item for item in allData if item['sitename']==add_selectbox]
+st.table(data=selected_item)
