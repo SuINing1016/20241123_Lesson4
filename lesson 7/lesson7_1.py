16b4ce52e117cabcb78071a01e3c6b6ca3661d46
SuINing <IningSu@gmail.com>
2024-12-14 14:18:36 +0800

    lesson7.6

--- lesson 7/lesson7_1.py
+++ lesson 7/lesson7_1.py
@@ -1,21 +1,9 @@
-import requests
-from requests import Response
-from io import StringIO
-from csv import DictReader
-from requests.exceptions import RequestException,HTTPError
-url='https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
+import tools
 try:
-    r= requests.request('GET',url)
-    r.raise_for_status()
-except HTTPError as e:
-    print(e)
-except RequestException as e:
+    youbike_data:list[dict]=tools.get_youbikes()
+except Exception as e:
     print(e)
 else:
-    print('下載成功')
-    file=StringIO(r.text)
-    reader=DictReader(file)
-    list_reader=list(reader)
-    print(list_reader)
+    print(youbike_data)
 
 
